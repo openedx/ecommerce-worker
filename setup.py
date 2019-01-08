47676024cc50a24b8212876f99f38a6865e36adf
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2019-01-08 08:26:32 -0500

    Revert "updated version to 0.7.1"
    This reverts commit c12ede460760f9d86302e09a396ebc2cc96485f4.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as readme:
 
 setup(
     name='edx-ecommerce-worker',
-    version='0.7.1',
+    version='0.7.0',
     description='Celery tasks supporting the operations of edX\'s ecommerce service',
     long_description=long_description,
     classifiers=[
