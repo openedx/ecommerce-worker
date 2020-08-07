08dd623b98e9ce8a18506628b245a75c883749c5
Hasnain Naveed <hasnain.naveed123@gmail.com>
2020-08-07 16:53:39 +0500

    ENT-3280 | Added the already existed template for "send_offer_usage_email" task. (#70)

--- ecommerce_worker/configuration/base.py
+++ ecommerce_worker/configuration/base.py
@@ -96,6 +96,5 @@ SAILTHRU = {
     'templates': {
         'course_refund': 'Course Refund',
         'assignment_email': 'Offer Assignment Email',
-        'usage_email': 'Offer Usage Email',
     }
 }
