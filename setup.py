d2c9856f259504827a965edd1beebd6367e2d7fa
Calen Pennington <cale@edx.org>
2020-08-14 15:33:21 -0400

    Bump version to 0.8.1 to pass the relaxed redis version through

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def is_requirement(line):
 
 setup(
     name='edx-ecommerce-worker',
-    version='0.8.0',
+    version='0.8.1',
     description='Celery tasks supporting the operations of edX\'s ecommerce service',
     long_description=long_description,
     classifiers=[
