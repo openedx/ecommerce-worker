6ac56a8e280b085b4faefc7b3c121dab03f021a1
Diane Kaplan <dkaplan@edx.org>
2020-09-16 08:57:16 -0400

    update ecommerce worker version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def is_requirement(line):
 
 setup(
     name='edx-ecommerce-worker',
-    version='0.8.2',
+    version='0.8.3',
     description='Celery tasks supporting the operations of edX\'s ecommerce service',
     long_description=long_description,
     classifiers=[
