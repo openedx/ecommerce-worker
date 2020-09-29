39a784bed791126b35de72c2766377ba8f363054
Lael Birch <lbirch@edx.org>
2020-09-29 12:15:24 -0400

    Add enterprise url to offers emails

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def is_requirement(line):
 
 setup(
     name='edx-ecommerce-worker',
-    version='0.8.6',
+    version='0.8.7',
     description='Celery tasks supporting the operations of edX\'s ecommerce service',
     long_description=long_description,
     classifiers=[
