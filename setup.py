56401adc5c168e2415d2d37c35cac1d30d6c3398
Usama Sadiq <usama.sadiq@arbisoft.com>
2021-10-08 19:01:07 +0500

    chore: bump the version to 3.0.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def is_requirement(line):
 
 setup(
     name='edx-ecommerce-worker',
-    version='2.3.0',
+    version='3.0.0',
     description='Celery tasks supporting the operations of edX\'s ecommerce service',
     long_description=long_description,
     classifiers=[
