e83499cd01715a63565f050c41134a2472e3e30c
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2024-04-23 08:59:59 -0400

    fix: nit

--- ecommerce_worker/celery_app.py
+++ ecommerce_worker/celery_app.py
@@ -5,7 +5,7 @@ from ecommerce_worker.configuration import CONFIGURATION_MODULE
 
 
 # TEMP: This code will be removed by ARCH-BOM on 4/22/24
-# ddtrace allows celery task logs to be traced by the dd agent.
+# ddtrace allows celery task logs to be traced by the dd agent
 # TODO: remove this code.
 try:
     from ddtrace import patch
